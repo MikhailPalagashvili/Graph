764f4d5e2386c108fbcc751ae7f55a7b8871d2b2
Mikhail Palagashvili <48160170+MikhailPalagashvili@users.noreply.github.com>
2019-03-09 14:15:38 -0500

    Update ConnectedComponents.java

--- ConnectedComponents.java
+++ ConnectedComponents.java
@@ -34,4 +34,20 @@ public class ConnectedComponents {
     public int getId(int vertex) {
         return id[vertex];
     }
+
+    public static void main(String[] args) {
+        final int vertices = 13;
+        final int source = 0;
+        Graph graph = new Graph(vertices);
+        graph.addEdge(0,1); graph.addEdge(0,2);
+        graph.addEdge(0,6); graph.addEdge(0,5);
+        graph.addEdge(5,3); graph.addEdge(5,4);
+        graph.addEdge(3,4); graph.addEdge(4,6);
+        graph.addEdge(7,8); graph.addEdge(9,10);
+        graph.addEdge(9,12); graph.addEdge(9,11); graph.addEdge(11,12);
+        ConnectedComponents cc = new ConnectedComponents(graph);
+        System.out.println("There are " + cc.getCount() + " connected components in this graph");
+        System.out.println("*****************");
+        for (int v = 0; v < graph.getVertices(); v++) System.out.println(cc.getId(v));
+    }
 }
