96f5f28d70850c37a309a49da2cace63a1b17f04
Mikhail Palagashvili <48160170+MikhailPalagashvili@users.noreply.github.com>
2019-03-09 14:07:18 -0500

    Update DFS.java

--- DFS.java
+++ DFS.java
@@ -33,4 +33,18 @@ public class DFS {
         path.push(source);
         return path;
     }
+
+    public static void main(String[] args) {
+        final int vertices = 13;
+        final int source = 0;
+        Graph graph = new Graph(vertices);
+        graph.addEdge(0,1); graph.addEdge(0,2);
+        graph.addEdge(0,6); graph.addEdge(0,5);
+        graph.addEdge(5,3); graph.addEdge(5,4);
+        graph.addEdge(3,4); graph.addEdge(4,6);
+        graph.addEdge(7,8); graph.addEdge(9,10);
+        graph.addEdge(9,12); graph.addEdge(9,11); graph.addEdge(11,12);
+        DFS paths = new DFS(graph, source);
+        for (int v = 0; v < graph.getVertices(); v++) if (paths.hasPathTo(v)) System.out.println(paths.pathTo(v));
+    }
 }
